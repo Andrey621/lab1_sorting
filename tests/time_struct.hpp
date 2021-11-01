1a9795bcca68e0c67b26bb30297edead5be8d5e7
Andrey621 <84589915+Andrey621@users.noreply.github.com>
2021-11-01 19:04:51 +0300

    Create time_struct.hpp

---
+++ tests/time_struct.hpp
@@ -0,0 +1,10 @@
+//это структура для времени работы каждой из сортировок
+#pragma once
+struct for_time{
+    float bubble_sort_time;
+    float quick_sort_time;
+    float shell_sort_time;
+    float shaker_sort_time;
+    float insert_sort_time;
+    float merge_sort_time;
+};
