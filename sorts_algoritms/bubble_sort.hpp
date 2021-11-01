0dc4c9f1920a154c9da89f0a52bc406a0a0f8bf7
Andrey621 <84589915+Andrey621@users.noreply.github.com>
2021-11-01 18:10:15 +0300

    Update bubble_sort.hpp

--- sorts_algoritms/bubble_sort.hpp
+++ sorts_algoritms/bubble_sort.hpp
@@ -5,10 +5,9 @@ void BubbleSort(Sequence<T>* c){
     for (int i = 0; i < c->GetSize(); i++) {
         for (int j = (c->GetSize() - 1); j > i; j--) {
             if ((*c)[j - 1] > (*c)[j]) {
-                T temp = (*c)[j - 1];
-                (*c)[j - 1] = (*c)[j];
-                (*c)[j] = temp;
+                swap((*c)[j - 1], (*c)[j]);
             }
         }
     }
 }
+
